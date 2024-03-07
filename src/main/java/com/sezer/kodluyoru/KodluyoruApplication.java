5478d9759928e887d5245c0d1af12e74f3483cc9
Sezer Bozkurt <sezer.bozkurt@vbt.com.tr>
2024-03-07 13:55:44 +0300

    first commit

---
+++ src/main/java/com/sezer/kodluyoru/KodluyoruApplication.java
@@ -0,0 +1,14 @@
+package com.sezer.kodluyoru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodluyoruApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(KodluyoruApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sezer/kodluyoru/KodluyoruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sezer.kodluyoru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodluyoruApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
